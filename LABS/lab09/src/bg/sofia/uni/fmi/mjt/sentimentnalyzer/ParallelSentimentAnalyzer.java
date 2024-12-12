1f39b41247da81809b9a5ba6b7a9827d7cce5477
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2024-12-12 17:05:06 +0200

    add lab09

---
+++ LABS/lab09/src/bg/sofia/uni/fmi/mjt/sentimentnalyzer/ParallelSentimentAnalyzer.java
@@ -0,0 +1,21 @@
+package bg.sofia.uni.fmi.mjt.sentimentnalyzer;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ParallelSentimentAnalyzer implements SentimentAnalyzerAPI{
+
+    /**
+     * @param workersCount number of consumer workers
+     * @param stopWords set containing stop words
+     * @param sentimentLexicon map containing the sentiment lexicon, where the key is the word and the value is the sentiment score
+     */
+    public ParallelSentimentAnalyzer(int workersCount, Set<String> stopWords, Map<String,Integer> sentimentLexicon) {
+        
+    }
+
+    @Override
+    public Map<String, SentimentScore> analyze(AnalyzerInput... input) {
+        return Map.of();
+    }
+}
---
+++ LABS/lab09/src/bg/sofia/uni/fmi/mjt/sentimentnalyzer/exceptions/SentimentAnalysisException.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.sentimentnalyzer.exceptions;
+
+public class SentimentAnalysisException {
+}

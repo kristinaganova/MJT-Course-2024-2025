6fdec012347d4331e7c071de27cb0edee5b7bae3
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2025-01-12 14:04:02 +0200

    rearange

--- LABS/lab07/src/bg/sofia/uni/fmi/mjt/imagekit/algorithm/blur/BlurAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.imagekit.algorithm.blur;
-
-import bg.sofia.uni.fmi.mjt.imagekit.algorithm.ImageAlgorithm;
-
-public interface BlurAlgorithm extends ImageAlgorithm { }
--- LABS/lab08/src/bg/sofia/uni/fmi/mjt/frauddetector/transaction/Channel.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.frauddetector.transaction;
-
-public enum Channel {
-    ATM, ONLINE, BRANCH
-}
